Add Embedding layer and fix Attention mask bug

MyTorch/layers/attention.py
@@ -1,4 +1,4 @@
-from MyTorch import Model, Parameter
+from MyTorch import Model, Tensor
 from . import Linear, Dropout
 from ..activations import Softmax
 import numpy as np
@@ -13,7 +13,7 @@ class Attention(Model):
         self.v_linear = Linear(embed_dim, embed_dim)
         self.softmax = Softmax(dim=2)
 
-    def forward(self, q, k, v, mask=None):
+    def forward(self, q, k, v, mask: Tensor = None):
         # q: (batch_size, seq_len, embed_dim)
         # k: (batch_size, seq_len, embed_dim)
         # v: (batch_size, seq_len, embed_dim)
@@ -23,7 +23,14 @@ class Attention(Model):
         # q: (batch_size, seq_len, embed_dim)
         # k: (batch_size, seq_len, embed_dim)
         # v: (batch_size, seq_len, embed_dim)
-        scores = self.softmax(q @ k.transpose(0, 2, 1) / np.sqrt(self.embed_dim))
+        scaled_attention_logits = q @ k.transpose(0, 2, 1) / np.sqrt(self.embed_dim)
+        # scaled_attention_logits: (batch_size, seq_len, seq_len)
+        if mask != None:
+            # mask: (batch_size, seq_len)
+            _mask = mask.reshape(mask.shape[0], 1, -1)
+            _inverse_mask = _mask * -1 + 1
+            scaled_attention_logits = scaled_attention_logits + (_inverse_mask * -1e9)
+        scores = self.softmax(scaled_attention_logits)
         # scores: (batch_size, seq_len, seq_len)
         scores = self.dropout(scores)
         context = scores @ v
@@ -33,6 +40,7 @@ class Attention(Model):
 class MultiheadAttention(Model):
     def __init__(self, embed_dim, num_heads, dropout=0.0):
         super().__init__()
+        assert embed_dim % num_heads == 0
         self.embed_dim = embed_dim
         self.num_heads = num_heads
         self.dropout = Dropout(dropout)
@@ -41,7 +49,7 @@ class MultiheadAttention(Model):
         self.v_linear = Linear(embed_dim, embed_dim)
         self.softmax = Softmax(dim=3)
 
-    def forward(self, q, k, v, mask=None):
+    def forward(self, q, k, v, mask: Tensor = None):
         # q: (batch_size, seq_len, embed_dim)
         # k: (batch_size, seq_len, embed_dim)
         # v: (batch_size, seq_len, embed_dim)
@@ -55,11 +63,17 @@ class MultiheadAttention(Model):
         # q: (batch_size, num_heads, seq_len, embed_dim // num_heads)
         # k: (batch_size, num_heads, seq_len, embed_dim // num_heads)
         # v: (batch_size, num_heads, seq_len, embed_dim // num_heads)
-        scores = self.softmax(q @ k.transpose(0, 1, 3, 2) / np.sqrt(self.embed_dim / self.num_heads))
+        scaled_attention_logits = q @ k.transpose(0, 1, 3, 2) / np.sqrt(self.embed_dim / self.num_heads)
+        # scaled_attention_logits: (batch_size, num_heads, seq_len, seq_len)
+        if mask != None:
+            # mask: (batch_size, seq_len)
+            _mask = mask.reshape(mask.shape[0], 1, 1, -1)
+            _inverse_mask = _mask * -1 + 1
+            scaled_attention_logits = scaled_attention_logits + (_inverse_mask * -1e9)
+        scores = self.softmax(scaled_attention_logits)
         # scores: (batch_size, num_heads, seq_len, seq_len)
         scores = self.dropout(scores)
         context = scores @ v
         # context: (batch_size, num_heads, seq_len, embed_dim // num_heads)
         context = context.transpose(0, 2, 1, 3).reshape(batch_size, -1, self.embed_dim)
         return context
-    

MyTorch/layers/linear.py
@@ -6,8 +6,16 @@ from MyTorch import Parameter
 class Linear(Model):
     def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.weights = Parameter(np.random.randn(input_dim, output_dim) * np.sqrt(2.0 / input_dim))
-        self.bias = Parameter(np.random.randn(1, output_dim) * np.sqrt(2.0 / input_dim))
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.weights = Parameter(np.empty((input_dim, output_dim)))
+        self.bias = Parameter(np.empty((1, output_dim)))
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+        # use Kaiming initialization
+        self.weights.data = np.random.randn(self.input_dim, self.output_dim) * np.sqrt(2 / self.input_dim)
+        self.bias.data = np.random.randn(1, self.output_dim) * np.sqrt(2 / self.input_dim)
         
     def forward(self, x):
         return MatMul()(x, self.weights) + self.bias